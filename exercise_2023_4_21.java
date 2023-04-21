915b73424667b81c2def3363b1d18366949be0a3
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-04-21 10:08:01 +0800

    Add files via upload

---
+++ exercise_2023_4_21.java
@@ -0,0 +1,32 @@
+
+////L1-045 宇宙无敌大招呼
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner scan = new Scanner(System.in);
+//        String str = scan.nextLine();
+//        System.out.println("Hello "+str);
+//        scan.close();
+//    }
+//}
+
+
+////L1-047 装睡
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            String name = scan.next();
+//            int h = scan.nextInt();
+//            int m = scan.nextInt();
+//            if ((h >= 15 && h <= 20) && (m >= 50 && m <= 70)) {
+//                continue;
+//            }
+//            System.out.println(name);
+//        }
+//    }
+//}
