e975edd1925485c3bf2194af1845bb0984128c74
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-04-24 00:28:00 +0800

    Add files via upload

--- test_2023_4_22/src/Test.java
+++ test_2023_4_22/src/Test.java
@@ -1,7 +1,56 @@
+public class Test {
+    public static void main1(String[] args) {
+
+        {
+            int x = 10;
+            System.out.println(x);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Student.classRoom = "109";
+        System.out.println(Student.classRoom);
+        Student student = new Student("张三", 19);
+        System.out.println(student);
+
+    }
+}
+
 class Student {
     public String name;
     public int age;
 
+    public static String classRoom;
+
+    {//实例代码块，用来初始化普通成员变量的
+        this.name = "mika";
+        this.age = 17;
+        System.out.println("实例代码块");
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    static {//静态代码块，用来初始化静态成员变量的
+        classRoom = "109";
+    }
+
+    public void show() {
+        System.out.println("姓名：" + this.name + " 年龄：" + this.age);
+    }
+
+    public static void fun() {
+        System.out.println("这是一个静态方法");
+        //show();//err 不能在静态方法内部直接调用非静态方法/成员方法
+        Student student = new Student("A", 19);
+        student.show();
+    }
 //    public String getName() {
 //        return name;
 //    }
@@ -23,26 +72,26 @@ class Student {
 //        this.age = age;
 //    }
 
-    public Student(int age) {
-        this.age = age;
-    }
-
-    public Student(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Student() {
-        //this("爱抖露", 17);
-        System.out.println("这是一个没有参数的构造方法");
-    }
+//    public Student(int age) {
+//        this.age = age;
+//    }
+//
+//    public Student(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return this.name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Student() {
+//        //this("爱抖露", 17);
+//        System.out.println("这是一个没有参数的构造方法");
+//    }
 
     public Student(String name, int age) {
         //this();
@@ -51,23 +100,23 @@ class Student {
         System.out.println("这是一个有两个参数的构造方法");
     }
 
-    public void show() {
-        System.out.println("姓名：" + this.name + " 年龄：" + this.age);
-    }
+//    public void show() {
+//        System.out.println("姓名：" + this.name + " 年龄：" + this.age);
+//    }
 
-    public void eat() {
-        System.out.println(this.name + "在吃饭");
-        this.show();
-    }
+//    public void eat() {
+//        System.out.println(this.name + "在吃饭");
+//        this.show();
+//    }
 }
 
-public class Test {
-    public static void main(String[] args) {
-        Student student = new Student();
-        student.setName("xila");
-        System.out.println(student.getName());
-        //student.name = "xila";
-        //Student student2 = new Student("zhangsan", 18);
-    }
-}
+//public class Test {
+//    public static void main(String[] args) {
+//        Student student = new Student();
+////        student.setName("xila");
+////        System.out.println(student.getName());
+//        //student.name = "xila";
+//        //Student student2 = new Student("zhangsan", 18);
+//    }
+//}
 
