e5b89792db4c55665b58c5e17ea5315448aa1213
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-04-29 20:16:23 +0800

    Add files via upload

---
+++ exercise_2023_4_29.java
@@ -0,0 +1,18 @@
+
+
+
+//L1-084 拯救外星人
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int A = scan.nextInt();
+//        int B = scan.nextInt();
+//        int ret = 1;
+//        for (int i = 1; i <= A + B; i++) {
+//            ret *= i;
+//        }
+//        System.out.println(ret);
+//    }
+//}
