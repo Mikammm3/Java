579e4e1618d467c4b7959601d6c3af69f062d3ed
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-01 21:26:36 +0800

    Add files via upload

---
+++ exercise_2023_5_1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class exercise_2023_5_1 {
+
+}
+
+
+//PTA L1-076 降价提醒机器人
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int N = scan.nextInt();
+//        int M = scan.nextInt();
+//        for (int i = 0; i < N; i++) {
+//            double d = scan.nextDouble();
+//            if ((int) d < M) {
+//                System.out.println("On Sale! " + d);
+//            }
+//        }
+//    }
+//}
+
+
+//PTA L1-077 大笨钟的心情
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String args[]) {
+//        Scanner scan = new Scanner(System.in);
+//        int arr[] = new int[24];
+//        for (int i = 0; i < 24; i++) {
+//            arr[i] = scan.nextInt();
+//        }
+//        while (true) {
+//            int n = scan.nextInt();
+//            if (n >= 0 && n <= 23) {
+//                if (arr[n] > 50) {
+//                    System.out.println(arr[n] + " Yes");
+//                } else {
+//                    System.out.println(arr[n] + " No");
+//                }
+//            } else {
+//                break;
+//            }
+//        }
+//    }
+//}
