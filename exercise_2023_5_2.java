1b6cdb295579dd347f03550e6b088ff5318f1e54
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-02 20:16:57 +0800

    Add files via upload

---
+++ exercise_2023_5_2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//public class exercise_2023_5_2 {
+//
+//}
+
+//PTA L1-081 今天我要赢
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("I'm gonna win! Today!");
+//        System.out.println("2022-04-23");
+//    }
+//}
+
+
+//PTA L1-082 种钻石
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int N = scan.nextInt();
+//        int v = scan.nextInt();
+//        System.out.println(N / v);
+//        scan.close();
+//    }
+//}
