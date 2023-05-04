7f0fa022cbe9da2e9ea600fcbbd2e2a7d9236b94
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-04 21:55:56 +0800

    Add files via upload

---
+++ exercise_2023_5_4.java
@@ -0,0 +1,28 @@
+//import java.util.Scanner;
+//
+//public class exercise_2023_5_4 {
+//}
+
+
+//PTA L1-089 最好的文档
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Good code is its own best documentation.");
+//    }
+//}
+
+
+//PTA L1-090 什么是机器学习
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        int b = scan.nextInt();
+//        System.out.println(a + b - 16);
+//        System.out.println(a + b - 3);
+//        System.out.println(a + b - 1);
+//        System.out.println(a + b);
+//        scan.close();
+//    }
+//}
