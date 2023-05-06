3884ef1fe22b1f64ab59b91dbc7ec382bf8a9e8f
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-06 21:37:07 +0800

    Add files via upload

---
+++ exercise_2023_5_6.java
@@ -0,0 +1,21 @@
+
+
+
+//public class exercise_2023_5_5 {
+//
+//}
+
+
+//PTA L1-060 心理阴影面积
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int x = scan.nextInt();
+//        int y = scan.nextInt();
+//        int a = 100 - x;
+//        int b = 100 - y;
+//        System.out.println(100 * 100 / 2 - x * y / 2 - a * b / 2 - a * y);
+//        scan.close();
+//    }
+//}
