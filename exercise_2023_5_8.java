2b7ef5f4c861cc0eb88ab68aef48a70abbc7dd8a
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-08 12:31:33 +0800

    Add files via upload

---
+++ exercise_2023_5_8.java
@@ -0,0 +1,22 @@
+
+
+
+//LeetCode 338. 比特位计数
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            ans[i] = getOneCount(i);
+        }
+        return ans;
+    }
+
+    public static int getOneCount(int n) {
+        int count = 0;
+        while (n != 0) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+}
