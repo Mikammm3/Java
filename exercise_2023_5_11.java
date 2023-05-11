90b59b56cd2b2c57e190889d884c0c0652039504
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-11 15:36:51 +0800

    Add files via upload

---
+++ exercise_2023_5_11.java
@@ -0,0 +1,11 @@
+
+//LeetCode 136. 只出现一次的数字
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ret ^= nums[i];
+        }
+        return ret;
+    }
+}
