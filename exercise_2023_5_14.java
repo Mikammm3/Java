ea53fb20b5e0d3e89d0751027ab1e37f791a67fa
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-14 17:53:22 +0800

    Add files via upload

---
+++ exercise_2023_5_14.java
@@ -0,0 +1,69 @@
+
+
+//LeetCode387. 字符串中的第一个唯一字符
+//class Solution {
+//    public int firstUniqChar(String s) {
+//        int[] arr = new int[26];
+//        for (int i = 0; i < s.length(); i++) {
+//            char ch = s.charAt(i);
+//            arr[ch - 'a']++;
+//        }
+//        for (int i = 0; i < s.length(); i++) {
+//            char ch = s.charAt(i);
+//            if (arr[ch - 'a'] == 1) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+//}
+
+
+//牛客网 HJ1 字符串最后一个单词的长度
+//import java.util.Scanner;
+//
+//// 注意类名必须为 Main, 不要有任何 package xxx 信息
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        String str = scan.nextLine();
+//        String[] arr = str.split(" ");
+//        int len = arr.length - 1;
+//        System.out.println(arr[len].length());
+//
+//    }
+//}
+
+
+//LeetCode125. 验证回文串
+//class Solution {
+//    public static boolean isLegal(char ch) {
+//        if (ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9') {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    public boolean isPalindrome(String str) {
+//        str = str.toLowerCase();
+//        char[] arr = str.toCharArray();
+//        int left = 0;
+//        int right = arr.length - 1;
+//        while (left < right) {
+//            while (left < right && !isLegal(arr[left])) {
+//                left++;
+//            }
+//            while (left < right && !isLegal(arr[right])) {
+//                right--;
+//            }
+//            if (arr[left] != arr[right]) {
+//                return false;
+//            } else {
+//                left++;
+//                right--;
+//            }
+//        }
+//        return true;
+//    }
+//}
