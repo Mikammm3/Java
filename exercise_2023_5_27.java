92bea2143dea1685dc97a9c96656fe5aeac7d0e4
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-27 19:52:40 +0800

    Add files via upload

---
+++ exercise_2023_5_27.java
@@ -0,0 +1,21 @@
+public class exercise_2023_5_27 {
+}
+
+
+//牛客网 BM1 反转链表
+//public class Solution {
+//    public ListNode ReverseList(ListNode head) {
+//        if (head == null) {
+//            return null;
+//        }
+//        ListNode cur = head.next;
+//        head.next = null;
+//        while (cur != null) {
+//            ListNode curNext = cur.next;
+//            cur.next = head;
+//            head = cur;
+//            cur = curNext;
+//        }
+//        return head;
+//    }
+//}
