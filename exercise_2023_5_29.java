1569dc168f62c61c88bc05163b2f0083073e3bef
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-05-29 22:14:07 +0800

    Add files via upload

---
+++ exercise_2023_5_29.java
@@ -0,0 +1,20 @@
+
+
+//牛客网 BM6 判断链表中是否有环
+//public class Solution {
+//    public boolean hasCycle(ListNode head) {
+//        if (head == null) {
+//            return false;
+//        }
+//        ListNode fast = head;
+//        ListNode slow = head;
+//        while (fast != null && fast.next != null) {
+//            fast = fast.next.next;
+//            slow = slow.next;
+//            if (fast == slow) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
