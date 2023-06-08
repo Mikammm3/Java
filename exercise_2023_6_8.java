1cd73fee71f1f042eb4ae608f8c8739e5778799d
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-06-08 18:00:43 +0800

    Add files via upload

---
+++ exercise_2023_6_8.java
@@ -0,0 +1,13 @@
+
+//牛客网 BM51 数组中出现次数超过一半的数字
+//import java.util.*;
+//
+//public class Solution {
+//    public int MoreThanHalfNum_Solution(int[] array) {
+//        if (array == null || array.length == 0) {
+//            return 0;
+//        }
+//        Arrays.sort(array);
+//        return array[array.length / 2];
+//    }
+//}
