14eba8bb0d071e3b1795a1859afc49e37c908502
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-06-14 21:13:29 +0800

    Add files via upload

---
+++ exercise_2023_6_14.java
@@ -0,0 +1,16 @@
+
+//牛客网 BM62 斐波那契数列
+//public class Solution {
+//    public int Fibonacci(int n) {
+//        int a = 1;
+//        int b = 1;
+//        int c = 1;
+//        while (n > 2) {
+//            c = a + b;
+//            a = b;
+//            b = c;
+//            n--;
+//        }
+//        return c;
+//    }
+//}
