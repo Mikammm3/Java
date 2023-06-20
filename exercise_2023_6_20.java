9a548ea6cd484715c6fd7294a1034d3d246fd324
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-06-20 20:55:28 +0800

    Add files via upload

---
+++ exercise_2023_6_20.java
@@ -0,0 +1,28 @@
+
+
+//牛客网 NC140 排序
+//import java.util.*;
+//
+//
+//public class Solution {
+//    /**
+//     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//     * <p>
+//     * 将给定数组排序
+//     *
+//     * @param arr int整型一维数组 待排序的数组
+//     * @return int整型一维数组
+//     */
+//    public int[] MySort(int[] arr) {
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            for (int j = 0; j < arr.length - 1 - i; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    int tmp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = tmp;
+//                }
+//            }
+//        }
+//        return arr;
+//    }
+//}
