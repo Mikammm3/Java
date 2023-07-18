1639f1f7b18eb072b14a7a82f0f129c57427f086
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-07-18 23:06:05 +0800

    Add files via upload

---
+++ test_2023_5_29/src/BinaryTree.java
@@ -0,0 +1,17 @@
+
+
+public class BinaryTree {
+    static class TreeNode {
+        public char val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(char val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode root;//根节点
+
+    
+}
