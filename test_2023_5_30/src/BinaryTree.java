11cee35db9eeb4ea7daf228cedccd161c1f13c90
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-07-28 23:16:08 +0800

    Add files via upload

--- test_2023_5_30/src/BinaryTree.java
+++ test_2023_5_30/src/BinaryTree.java
@@ -164,7 +164,21 @@ public class BinaryTree {
         return A;
     }
 
-
+    public boolean judge(String str) {
+        if (str == null) {
+            return true;
+        }
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
 
 //class Solution {
