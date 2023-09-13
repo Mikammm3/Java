32ba10b2c86aebed6f3c194508a1782e2d2b3414
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-09-13 22:01:05 +0800

    Add files via upload

---
+++ exercise_2023_9_13.java
@@ -0,0 +1,19 @@
+
+// 牛客网 JZ43 整数中1出现的次数（从1到n整数中1出现的次数）
+//import java.util.*;
+//
+//public class Solution {
+//    public int NumberOf1Between1AndN_Solution(int n) {
+//        int count = 0;
+//        for (int i = 1; i <= n; i++) {
+//            String str = String.valueOf(i);
+//            char[] arr = str.toCharArray();
+//            for (int j = 0; j < arr.length; j++) {
+//                if (arr[j] == '1') {
+//                    count++;
+//                }
+//            }
+//        }
+//        return count;
+//    }
+//}
