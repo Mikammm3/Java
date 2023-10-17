b5059195188013aee89c12f462bf2997bf4f7e00
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-10-17 22:24:36 +0800

    Add files via upload

---
+++ exercise_2023_10_17.java
@@ -0,0 +1,18 @@
+
+
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        while (scanner.hasNextInt()) {
+//            int n = scanner.nextInt();
+//            int count = 0;
+//            while (n != 0) {
+//                n &= (n - 1);
+//                count++;
+//            }
+//            System.out.println(count);
+//        }
+//    }
+//}
