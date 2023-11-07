f9b4c07da2441c96abaee57b46cd8756e04d7cb2
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-11-07 23:04:41 +0800

    Add files via upload

---
+++ hello_servlet/hello_servlet2/src/main/java/HelloServlet2.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //resp.setContentType("text/html; charset=utf8");
+        resp.setCharacterEncoding("GBK");
+        System.out.println("你好 java");
+        resp.getWriter().write("你好 java");
+    }
+}
