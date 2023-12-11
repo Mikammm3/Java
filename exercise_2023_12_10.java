373e3ddde0ff85a7ae8bd8b60e74defb02b3a7c5
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-12-11 23:34:06 +0800

    Add files via upload

--- exercise_2023_12_10.java
+++ exercise_2023_12_10.java
@@ -21,3 +21,47 @@
 //        return newHead.next;
 //    }
 //}
+
+
+// LeetCode 143. 重排链表
+//class Solution {
+//    public void reorderList(ListNode head) {
+//        if (head == null || head.next == null || head.next.next == null) return;
+//        // 1. 找中间节点
+//        ListNode fast = head, slow = head;
+//        while (fast != null && fast.next != null) {
+//            fast = fast.next.next;
+//            slow = slow.next;
+//        }
+//
+//        // 2. 翻转
+//        // prev 就是新头
+//        ListNode prev = slow, cur = slow.next;
+//        // 防止形成循环
+//        slow.next = null;
+//        while (cur != null) {
+//            ListNode curNext = cur.next;
+//            cur.next = prev;
+//            prev = cur;
+//            cur = curNext;
+//        }
+//
+//        // 3. 合并链表
+//        ListNode newHead = new ListNode(-1);
+//        newHead.next = head;
+//        cur = newHead; // cur 可以理解为 head
+//        while (prev != null && cur != null && head != prev) {
+//            ListNode headNext = head.next, prevNext = prev.next;
+//            // 先加头
+//            cur.next = head;
+//            // 往后走加 prev
+//            cur = cur.next;
+//            cur.next = prev;
+//            // 往后走
+//            cur = cur.next;
+//            head = headNext;
+//            prev = prevNext;
+//        }
+//        head = newHead.next;
+//    }
+//}
