3f43091d29720aaa60d1ec6c86c3e999b5d217fb
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2024-01-05 23:56:20 +0800

    Add files via upload

---
+++ exercise_2024_1_5.java
@@ -0,0 +1,14 @@
+
+
+// LeetCode 2331. 计算布尔二叉树的值
+//class Solution {
+//    public boolean evaluateTree(TreeNode root) {
+//        if (root.left == null && root.right == null) {
+//            return root.val == 0 ? false : true;
+//        }
+//        boolean left = evaluateTree(root.left);
+//        boolean right = evaluateTree(root.right);
+//        return (root.val == 2) ? (left || right) : (left && right);
+//
+//    }
+//}
