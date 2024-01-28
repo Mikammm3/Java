e5b1ec4b2c3c15c5ea51d1d44b6b8ba09a129478
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2024-01-28 18:37:30 +0800

    Add files via upload

---
+++ exercise_2024_1_28.java
@@ -0,0 +1,21 @@
+
+
+
+//import java.util.Scanner;
+//
+//// 注意类名必须为 Main, 不要有任何 package xxx 信息
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        // 注意 hasNext 和 hasNextLine 的区别
+//        while (in.hasNextLine()) { // 注意 while 处理多个 case
+//            String s = in.nextLine();
+//            for (char c : s.toCharArray()) {
+//                if (c >= '0' && c <= '9') {
+//                    System.out.print(c);
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
