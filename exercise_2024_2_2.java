854a3c65519607965bac091f1ceb219aacf88d14
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2024-02-02 20:33:48 +0800

    Add files via upload

---
+++ exercise_2024_2_2.java
@@ -0,0 +1,18 @@
+
+
+
+//import java.util.*;
+//
+//
+//public class Solution {
+//    /**
+//     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//     *
+//     * @param numbers int整型一维数组
+//     * @return int整型
+//     */
+//    public int MoreThanHalfNum_Solution(int[] numbers) {
+//        Arrays.sort(numbers);
+//        return numbers[numbers.length / 2];
+//    }
+//}
