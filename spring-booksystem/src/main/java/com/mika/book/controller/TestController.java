a70a4c50b50e01eaaba22721ddce0ddf80803b99
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2024-02-16 18:28:32 +0800

    Add files via upload

--- spring-booksystem/src/main/java/com/mika/book/controller/TestController.java
+++ spring-booksystem/src/main/java/com/mika/book/controller/TestController.java
@@ -13,11 +13,14 @@ public class TestController {
 
     @RequestMapping("/t2")
     public boolean t2() {
+        String a = null;
+        System.out.println(a.length());
         return true;
     }
 
     @RequestMapping("/t3")
     public String t3() {
+        int a = 1 / 0;
         return "t3";
     }
 }
