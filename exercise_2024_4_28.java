b23771f304950d2f15fd5b17024efb076de2231a
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2024-04-28 16:21:20 +0800

    Add files via upload

---
+++ exercise_2024_4_28.java
@@ -0,0 +1,28 @@
+
+
+//import java.util.*;
+//
+//// 注意类名必须为 Main, 不要有任何 package xxx 信息
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        // 注意 hasNext 和 hasNextLine 的区别
+//        while (in.hasNextLine()) { // 注意 while 处理多个 case
+//            String a = in.nextLine();
+//            String b = in.nextLine();
+//            Set<Character> set = new HashSet<>();
+//            // System.out.println(a);
+//            // System.out.println(b);
+//            for (char x : b.toCharArray()) {
+//                set.add(x);
+//            }
+//            for (char x : a.toCharArray()) {
+//                if (!set.contains(x)) {
+//                    System.out.print(x);
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
