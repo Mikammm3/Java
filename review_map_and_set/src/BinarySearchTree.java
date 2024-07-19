9c0b67e3a28087368bfccaad8b116e73c58c3ed2
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2024-07-19 14:47:52 +0800

    Add files via upload

--- review_map_and_set/src/BinarySearchTree.java
+++ review_map_and_set/src/BinarySearchTree.java
@@ -32,4 +32,36 @@ public class BinarySearchTree {
         return false;
     }
 
+    public boolean insert(int val) {
+        // node 一定是插入到叶子节点的孩子节点
+        TreeNode node = new TreeNode(val);
+        if (root == null) {
+            // 空树
+            root = node;
+            return true;
+        }
+        TreeNode cur = root;
+        TreeNode prev = null;// 指向 cur 的父亲节点
+        // cur 找插入位置
+        while (cur != null) {
+            if (node.val > cur.val) {
+                prev = cur;
+                cur = cur.right;
+            } else if (node.val < cur.val) {
+                prev = cur;
+                cur = cur.left;
+            } else {
+                // 二叉搜索树本身是不能存在相同的数据的
+                return false;
+            }
+        }
+        // 此时 cur 为空，比较 prev 和 node
+        if (node.val > prev.val) {
+            prev.right = node;
+        } else {
+            prev.left = node;
+        }
+        return true;
+    }
+
 }
--- review_map_and_set/src/Test.java
+++ review_map_and_set/src/Test.java
@@ -2,6 +2,15 @@
 
 public class Test {
     public static void main(String[] args) {
-        
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(70);
+        binarySearchTree.insert(59);
+        binarySearchTree.insert(90);
+        binarySearchTree.insert(37);
+        binarySearchTree.insert(63);
+        binarySearchTree.insert(83);
+        binarySearchTree.insert(120);
+        System.out.println(binarySearchTree.search(13));
+        System.out.println(binarySearchTree.search(83));
     }
 }
