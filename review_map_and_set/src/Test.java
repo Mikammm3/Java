ff986e463887004e57b40a9491d18656eed66f43
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2024-07-21 09:40:46 +0800

    Add files via upload

--- review_map_and_set/src/Test.java
+++ review_map_and_set/src/Test.java
@@ -1,7 +1,49 @@
-
+import java.util.*;
 
 public class Test {
+
     public static void main(String[] args) {
+
+        Set<String> set = new TreeSet<>();
+        set.add("abc");
+        set.add("idol");
+        set.add("hello");
+
+        System.out.println(set);
+
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+
+
+    public static void main2(String[] args) {
+        Map<String, Integer> map = new TreeMap<>();
+        TreeMap<String, Integer> map2 = new TreeMap<>();
+        map2.put("a", 3);
+        System.out.println();
+        map2.put("a", 8888);
+        map2.put("c", 2);
+        map2.put("d", 3);
+        System.out.println();
+
+//        int val = map2.get("a");
+//        System.out.println(val);
+//        int val2 = map2.getOrDefault("y", 8888);
+//        System.out.println(val2);
+//        // 得到所有 key
+//        Set<String> keys = map2.keySet();
+//        // 得到所有 value
+//        Collection<Integer> values = map2.values();
+
+//        Set<Map.Entry<String, Integer>> entries = map2.entrySet();
+//        for (Map.Entry<String, Integer> entry : entries) {
+//            System.out.println("key: " + entry.getKey() + ", value: " + entry.getValue());
+//        }
+
+    }
+
+    public static void main1(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
         binarySearchTree.insert(70);
         binarySearchTree.insert(59);
