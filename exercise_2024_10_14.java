d269684c4ae1d28fbf26e4bcc64eca0c9a96347a
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2024-10-14 22:39:35 +0800

    Add files via upload

---
+++ exercise_2024_10_14.java
@@ -0,0 +1,8 @@
+
+// leetcode 58. 最后一个单词的长度
+//class Solution {
+//    public int lengthOfLastWord(String s) {
+//        String[] arr = s.split(" ");
+//        return arr[arr.length - 1].length();
+//    }
+//}
